ac2738e45df19148b1ec44021f523ba30553375d
Tom Deakin <tom.deakin@bristol.ac.uk>
2021-01-27 12:33:40 +0000

    add nowaits to reduce barriers

--- mm_gpu_block.c
+++ mm_gpu_block.c
@@ -10,7 +10,7 @@
 
 void mm_gpu_block(int Ndim, int Mdim, int Pdim, TYPE *A, TYPE *B, TYPE *C){
   int i, j, k;
-  const int Bsize = 16;
+  const int Bsize = 8;
   int ib, jb, kb;
 
   /* Block size, must evenly divide the matrices */
@@ -34,33 +34,36 @@ void mm_gpu_block(int Ndim, int Mdim, int Pdim, TYPE *A, TYPE *B, TYPE *C){
 
       for (kb=0; kb<Pblk; kb++){
 
+#pragma omp parallel num_threads(Bsize*Bsize)
+{
         /* Copy block of A into pteam memory */
-        #pragma omp parallel for collapse(2)
+        #pragma omp for collapse(2) nowait
         for (i=ib*Bsize; i<((ib+1)*Bsize); i++){
           for(k=kb*Bsize; k<((kb+1)*Bsize); k++){
             Awrk[(i%Bsize)*Bsize + (k%Bsize)] = A[i*Pdim+k];
           }
         }
 
-        /* Copy block of B into pteam memory, transposing */
-        #pragma omp parallel for collapse(2)
+        /* Copy block of B into pteam memory */
+        #pragma omp for collapse(2)
         for (j=jb*Bsize; j<((jb+1)*Bsize); j++){
           for(k=kb*Bsize; k<((kb+1)*Bsize); k++){
-            Bwrk[(j%Bsize)*Bsize + (k%Bsize)] = B[k*Mdim+j];
+            Bwrk[(k%Bsize)*Bsize + (j%Bsize)] = B[k*Mdim+j];
           }
         }
 
 
         /* matrix multiply block */
-        #pragma omp parallel for collapse(2)
+        #pragma omp for collapse(2)
         for (i=ib*Bsize; i<((ib+1)*Bsize); i++){
           for (j=jb*Bsize; j<((jb+1)*Bsize); j++){
             for(k=kb*Bsize; k<((kb+1)*Bsize); k++){
               /* C(i,j) = sum(over k) A(i,k) * B(k,j) */
-              *(C+(i*Mdim+j)) += Awrk[(i%Bsize)*Bsize + (k%Bsize)] * Bwrk[(j%Bsize)*Bsize + (k%Bsize)];
+              *(C+(i*Mdim+j)) += Awrk[(i%Bsize)*Bsize + (k%Bsize)] * Bwrk[(k%Bsize)*Bsize + (j%Bsize)];
             }
           }
         }
+}
 
       }
     }
