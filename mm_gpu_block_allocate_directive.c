ed2b1f3fa3d859f3f8b7a85c40677048f9777253
Tom Deakin <tom.deakin@bristol.ac.uk>
2021-02-01 12:10:39 +0000

    Use inline integers, a small improvement in register count from 94 to  83

--- mm_gpu_block_allocate_directive.c
+++ mm_gpu_block_allocate_directive.c
@@ -11,8 +11,8 @@
 #define Bsize 8
 
 void mm_gpu_block(int Ndim, int Mdim, int Pdim, TYPE *A, TYPE *B, TYPE *C){
-  int i, j, k;
-  int ib, jb, kb;
+  //int i, j, k;
+  //int ib, jb, kb;
 
   /* Block size, must evenly divide the matrices */
   assert(Ndim % Bsize == 0);
@@ -33,37 +33,37 @@ void mm_gpu_block(int Ndim, int Mdim, int Pdim, TYPE *A, TYPE *B, TYPE *C){
   TYPE Bwrk[Bsize*Bsize];
 #pragma omp allocate(Awrk, Bwrk) allocator(omp_pteam_mem_alloc)
 
-#pragma omp distribute collapse(2)
- for (ib=0; ib < Nblk; ib++){ /* Loop over blocks of C. One team per block */
-    for (jb=0; jb < Mblk; jb++){
+#pragma omp distribute collapse(2) dist_schedule(static, 1)
+ for (int ib=0; ib < Nblk; ib++){ /* Loop over blocks of C. One team per block */
+    for (int jb=0; jb < Mblk; jb++){
 
-      for (kb=0; kb<Pblk; kb++){
+      for (int kb=0; kb<Pblk; kb++){
 
 #pragma omp parallel num_threads(Bsize*Bsize)
 {
         /* Copy block of A into pteam memory */
-        #pragma omp for collapse(2) nowait
-        for (i=ib*Bsize; i<((ib+1)*Bsize); i++){
-          for(k=kb*Bsize; k<((kb+1)*Bsize); k++){
+        #pragma omp for collapse(2) nowait schedule(static, 1)
+        for (int i=ib*Bsize; i<((ib+1)*Bsize); i++){
+          for(int k=kb*Bsize; k<((kb+1)*Bsize); k++){
             Awrk[(i%Bsize)*Bsize + (k%Bsize)] = A[i*Pdim+k];
           }
         }
 
         /* Copy block of B into pteam memory */
-        #pragma omp for collapse(2)
-        for (j=jb*Bsize; j<((jb+1)*Bsize); j++){
-          for(k=kb*Bsize; k<((kb+1)*Bsize); k++){
+        #pragma omp for collapse(2) schedule(static, 1)
+        for (int j=jb*Bsize; j<((jb+1)*Bsize); j++){
+          for(int k=kb*Bsize; k<((kb+1)*Bsize); k++){
             Bwrk[(k%Bsize)*Bsize + (j%Bsize)] = B[k*Mdim+j];
           }
         }
 
 
         /* matrix multiply block */
-        #pragma omp for collapse(2)
-        for (i=ib*Bsize; i<((ib+1)*Bsize); i++){
-          for (j=jb*Bsize; j<((jb+1)*Bsize); j++){
+        #pragma omp for collapse(2) schedule(static, 1)
+        for (int i=ib*Bsize; i<((ib+1)*Bsize); i++){
+          for (int j=jb*Bsize; j<((jb+1)*Bsize); j++){
             /*for(k=kb*Bsize; k<((kb+1)*Bsize); k++){*/
-            for (k=0; k < Bsize; ++k){
+            for (int k=0; k < Bsize; ++k){
               /* C(i,j) = sum(over k) A(i,k) * B(k,j) */
               *(C+(i*Mdim+j)) += Awrk[(i%Bsize)*Bsize + k] * Bwrk[k*Bsize + (j%Bsize)];
             }
